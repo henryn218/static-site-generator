3950895d1c75c2555c192f2582fdf98be448202f
henryn218 <henry.naish@gmail.com>
2024-10-07 23:36:44 +0000

    markdown to html node

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,9 @@
 import re
 
+from htmlnode import ParentNode, LeafNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
+
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -19,6 +23,86 @@ def block_to_block_type(block):
     elif all(x.startswith("- ") for x in lines):
         return "unordered_list"
     elif all(re.match(r"^\d\. ", x) for x in lines):
-        return "ordered_list"
+        for i, line in enumerate(lines):
+            if not line.startswith(f"{i + 1}. "):
+                break
+        else:
+            return "ordered_list"
+    return "paragraph"
+
+
+def markdown_to_html_node(markdown):
+    children_nodes = []
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == "heading":
+            children_nodes.append(make_heading_html_node(block))
+        elif block_type == "code":
+            code_node = LeafNode(block.strip("```").strip(), "code")
+            children_nodes.append(ParentNode("pre", [code_node]))
+        elif block_type == "quote":
+            children_nodes.append(make_quote_html_node(block))
+        elif block_type in ("unordered_list", "ordered_list"):
+            children_nodes.append(make_list_html_node(block, block_type))
+        elif block_type == "paragraph":
+            children_nodes.append(make_paragraph_html_node(block))
+
+    return ParentNode("div", children_nodes)
+
+
+def make_heading_html_node(block):
+    block_no_markdown = block.lstrip("#")
+    heading_tag = f"h{len(block) - len(block_no_markdown)}"
+    return LeafNode(block_no_markdown.strip(), heading_tag)
+
+
+def make_paragraph_html_node(block):
+    paragraph = " ".join(block.split("\n"))
+    return ParentNode("p", text_to_children(paragraph))
+
+
+def make_quote_html_node(block):
+    return ParentNode(
+        "blockquote",
+        text_to_children(block.lstrip(">").replace("\n>", "").strip()),
+    )
+
+
+def make_list_html_node(block, list_type):
+    if list_type == "unordered_list":
+        list_marker_stripper = strip_unordered_list_marker
+        list_tag = "ul"
+    elif list_type == "ordered_list":
+        list_marker_stripper = strip_ordered_list_marker
+        list_tag = "ol"
     else:
-        return "paragraph"
+        raise ValueError("List type must be one of 'unordered' or 'ordered'")
+
+    lines = block.split("\n")
+    stripped_lines = list_marker_stripper(lines)
+    list_nodes = []
+    for line in stripped_lines:
+        list_item_children = text_to_children(line)
+        list_nodes.append(ParentNode("li", list_item_children))
+    return ParentNode(list_tag, list_nodes)
+
+
+def strip_ordered_list_marker(lines):
+    return (text.lstrip(f"{i + 1}. ") for i, text in enumerate(lines))
+
+
+def strip_unordered_list_marker(lines):
+    if lines[0].startswith("*"):
+        list_marker = "* "
+    elif lines[0].startswith("- "):
+        list_marker = "- "
+    else:
+        raise ValueError("Unordered lists must use '-' or '*' to indicate list items")
+
+    return (text.strip(list_marker) for text in lines)
+
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    return [text_node_to_html_node(text_node) for text_node in text_nodes]
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,11 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks, block_to_block_type
+from markdown_blocks import (
+    markdown_to_blocks,
+    block_to_block_type,
+    markdown_to_html_node,
+)
+from htmlnode import ParentNode, LeafNode
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -86,3 +91,173 @@ class TestBlockMarkdown(unittest.TestCase):
 
     def test_block_to_block_type_with_empty_block(self):
         self.assertEqual(block_to_block_type(""), "paragraph")
+
+    def test_markdown_to_html_node(self):
+        actual = markdown_to_html_node("# Heading")
+        expected = ParentNode("div", [LeafNode("Heading", "h1")])
+        self.assertEqual(actual, expected)
+        self.assertEqual(actual.to_html(), "<div><h1>Heading</h1></div>")
+
+    def test_markdown_to_html_node_heading_and_list(self):
+        markdown = "## Heading\n\n* Item 1\n* Item 2\n* Item 3"
+        actual = markdown_to_html_node(markdown)
+        expected = ParentNode(
+            "div",
+            [
+                LeafNode("Heading", "h2"),
+                ParentNode(
+                    "ul",
+                    [
+                        ParentNode("li", [LeafNode("Item 1")]),
+                        ParentNode("li", [LeafNode("Item 2")]),
+                        ParentNode("li", [LeafNode("Item 3")]),
+                    ],
+                ),
+            ],
+        )
+        self.assertEqual(actual, expected)
+        self.assertEqual(
+            actual.to_html(),
+            "<div><h2>Heading</h2><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul></div>",
+        )
+
+    def test_markdown_to_html_node_heading_and_paragraphs(self):
+        markdown = "# Header 1\n\nThis is a paragraph under header 1.\n\n## Header 2\n\nAnother paragraph, with **bold** text and *italic* text."
+        actual = markdown_to_html_node(markdown)
+        expected = ParentNode(
+            "div",
+            [
+                LeafNode("Header 1", "h1"),
+                ParentNode("p", [LeafNode("This is a paragraph under header 1.")]),
+                LeafNode("Header 2", "h2"),
+                ParentNode(
+                    "p",
+                    [
+                        LeafNode("Another paragraph, with "),
+                        LeafNode("bold", "b"),
+                        LeafNode(" text and "),
+                        LeafNode("italic", "i"),
+                        LeafNode(" text."),
+                    ],
+                ),
+            ],
+        )
+        self.assertEqual(actual, expected)
+        self.assertEqual(
+            actual.to_html(),
+            "<div><h1>Header 1</h1><p>This is a paragraph under header 1.</p><h2>Header 2</h2><p>Another paragraph, with <b>bold</b> text and <i>italic</i> text.</p></div>",
+        )
+
+    def test_markdown_to_html_node_code_block(self):
+        markdown = "- Item in an unordered list\n- Another item\n\n1. First item in ordered list\n2. Second item\n\n```code block```"
+        actual = markdown_to_html_node(markdown)
+        expected = ParentNode(
+            "div",
+            [
+                ParentNode(
+                    "ul",
+                    [
+                        ParentNode("li", [LeafNode("Item in an unordered list")]),
+                        ParentNode("li", [LeafNode("Another item")]),
+                    ],
+                ),
+                ParentNode(
+                    "ol",
+                    [
+                        ParentNode("li", [LeafNode("First item in ordered list")]),
+                        ParentNode("li", [LeafNode("Second item")]),
+                    ],
+                ),
+                ParentNode("pre", [LeafNode("code block", "code")]),
+            ],
+        )
+        self.assertEqual(actual, expected)
+
+        self.assertEqual(
+            actual.to_html(),
+            "<div><ul><li>Item in an unordered list</li><li>Another item</li></ul><ol><li>First item in ordered list</li><li>Second item</li></ol><pre><code>code block</code></pre></div>",
+        )
+
+    def test_paragraph(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        print(node.to_html())
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with *italic* text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_lists(self):
+        md = """
+- This is a list
+- with items
+- and *more* items
+
+1. This is an `ordered` list
+2. with items
+3. and more items
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
+        )
+
+    def test_headings(self):
+        md = """
+# this is an h1
+
+this is paragraph text
+
+## this is an h2
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>this is an h1</h1><p>this is paragraph text</p><h2>this is an h2</h2></div>",
+        )
+
+    def test_blockquote(self):
+        md = """
+> This is a
+> blockquote block
+
+this is paragraph text
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
+        )
