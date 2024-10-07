5bfd62096a3941384ba0e99d1c4b53615e7677bf
henryn218 <henry.naish@gmail.com>
2024-10-07 05:41:13 +0000

    changed block to block type to only process one block at a time

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,24 @@
+import re
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     return [block.strip() for block in blocks if block.strip() != ""]
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    if block.startswith(tuple("#" * x + " " for x in range(1, 7))):
+        return "heading"
+    elif block.startswith("```") and block.endswith("```"):
+        return "code"
+    elif all(x.startswith(">") for x in lines):
+        return "quote"
+    elif all(x.startswith("* ") for x in lines):
+        return "unordered_list"
+    elif all(x.startswith("- ") for x in lines):
+        return "unordered_list"
+    elif all(re.match(r"^\d\. ", x) for x in lines):
+        return "ordered_list"
+    else:
+        return "paragraph"
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -55,3 +55,29 @@ class TestBlockMarkdown(unittest.TestCase):
         )
         expected = ["This is a single block\nwith multiple lines\nbut no empty lines"]
         self.assertEqual(actual, expected)
+
+    def test_block_to_block_type_with_heading(self):
+        self.assertEqual(block_to_block_type("###### This is a heading"), "heading")
+
+    def test_block_to_block_type_with_code(self):
+        self.assertEqual(block_to_block_type("```This is code\n```"), "code")
+
+    def test_block_to_block_type_with_unordered_list(self):
+        self.assertEqual(block_to_block_type("- item 1\n- item 2"), "unordered_list")
+
+    def test_block_to_block_type_with_ordered_list(self):
+        self.assertEqual(
+            block_to_block_type("1. item 1\n2. item 2\n3. item 3"), "ordered_list"
+        )
+
+    def test_block_to_block_type_with_paragraph(self):
+        self.assertEqual(block_to_block_type("here is some text"), "paragraph")
+
+    def test_block_to_block_type_with_incomplete_list(self):
+        actual = block_to_block_type(
+            "This is the first list item in an attempted list block\n2. This is a list item\n3. This is another list item",
+        )
+        self.assertEqual(actual, "paragraph")
+
+    def test_block_to_block_type_with_empty_block(self):
+        self.assertEqual(block_to_block_type(""), "paragraph")
