fbdd4de5958fd99cfbccecae30e3f54234e6a43e
henryn218 <henry.naish@gmail.com>
2024-10-08 20:01:30 +0000

    update main function to use content generation functions

--- src/main.py
+++ src/main.py
@@ -1,32 +1,13 @@
-import os
-import shutil
+from generate_content import copy_dir, generate_pages_recursive
 
 
 def main():
-    # text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    # print(text_node)
     copy_dir("static", "public")
-
-
-def copy_dir(source, destination):
-
-    if os.path.exists(destination):
-        for item in os.listdir(destination):
-            path = os.path.join(destination, item)
-            if os.path.isfile(path):
-                print(f"Deleting {path}")
-                os.remove(path)
-    else:
-        os.mkdir(destination)
-
-    for item in os.listdir(source):
-        source_path = os.path.join(source, item)
-        destination_path = os.path.join(destination, item)
-        print(f"Copying {source_path} -> {destination_path}")
-        if os.path.isfile(source_path):
-            shutil.copy(source_path, destination_path)
-        elif os.path.isdir(source_path):
-            copy_dir(source_path, destination_path)
+    generate_pages_recursive(
+        "content",
+        "template.html",
+        "public",
+    )
 
 
 if __name__ == "__main__":
